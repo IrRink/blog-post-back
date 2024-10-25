3bfabcc6fb60e11628b040bc458dd91be885ef81
ohkeonho <konhomaster@gmail.com>
2024-10-25 15:15:52 +0900

    ser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const static = require("serve-static");
 const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const pool = require("./models/pool"); // pool.js에서 pool을 불러옴
 const authRoutes = require("./routes/authRoutes");
@@ -21,8 +22,9 @@ const corsOptions = {
   origin: [
     "http://192.168.99.115:3000",
     "http://localhost:3000",
-    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5000",
   ],
+  methods: ["GET", "POST", "DELETE", "OPTIONS", "PUT"],
   credentials: true,
 };
 app.use(cors(corsOptions));
